8878a1658ab54e8b31718c263c69f3d68af09d5e
Venger-King <earthingout01@gmail.com>
2023-09-01 16:22:18 +0530

    glut added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include <windows.h>
+#include <GL/glut.h>
+// #include <iostream>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
+void display() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex2d(0.0f, -0.5f);
+    glVertex2d(-0.5f, -0.0f);
+    glVertex2d(0.5f, -0.0f);
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char **argv) {
+    // Code from Sample Site
+
+    glutInit(&argc, argv); // Initialize GLUT
+    glutCreateWindow(
+        "OpenGL Setup Test");     // Create a window with the given title
+    glutInitWindowSize(320, 320); // Set the window's initial width & height
+    glutInitWindowPosition(50,
+                           50); // Position the window's initial top-left corner
+    glutDisplayFunc(
+        display);   // Register display callback handler for window re-paint
+    glutMainLoop(); // Enter the event-processing loop
     return 0;
 }
